241cc9a29a7a05b152264ad17b7fd949116efe91
bitPogo <geislemx@informatik.hu-berlin.de>
2018-06-21 09:01:41 +0200

    Add system envs

---
+++ .test/index.spec.js
@@ -0,0 +1,28 @@
+/*require('../src/main.js');
+
+describe( 'index.js', () =>
+{
+    it( 'Grammar tests ', () =>
+    {
+        expect ("{{{}}} {} {{}}"._plFormatGrammarDebug()).toBe('');
+    });
+
+    it( 'Simple unformatted string', () =>
+    {
+        expect( "test".plFormat() ).toBe('test');
+        expect( "test".plFormat('why', 42, 2.3, "ever") ).toBe('test');
+        expect( "{{}}test".plFormat(['why', 42, 2.3, "ever"]) ).toBe('{}test');
+        expect( "test".plFormat({a:'why', b:42, c:2.3, d:"ever"}) ).toBe('test');
+    } );
+
+    it( 'Simple formatted string without format specification', () =>
+    {
+        expect( "{} tests {} are {} {}".plFormat() ).toBe('Why tests 42 are 2.3 ever');
+        expect( "{} tests {} are {}".plFormat() ).toBe('Why tests 42 are 2.3 ever');
+        expect( "{} tests {}".plFormat() ).toBe('Why tests 42 2.3 ever');
+        expect( "{} tests {} are {} {}".plFormat(['why', 42, 2.3, "ever"]) ).toBe('Why tests 42 are 2.3 ever');
+        expect( "test".plFormat({a:'why', b:42, c:2.3, d:"ever"}) ).toThrow('^\'Cannot switch from automatic field numbering to manual field specification.\'$');
+    } );
+} );*/
+"use strict";
+//# sourceMappingURL=index.spec.js.map
